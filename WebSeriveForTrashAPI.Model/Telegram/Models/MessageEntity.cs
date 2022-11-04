2c397bbfdf4642a72f7eae3f5f58a10395287faa
watermelonrytp@gmail.com <Felix>
2022-11-05 00:43:58 +0300

    Model layer

---
+++ WebSeriveForTrashAPI.Model/Telegram/Models/MessageEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebSeriveForTrashAPI.Model.Telegram.Models
+{
+    public record MessageEntity(
+         [property: JsonPropertyName("type")] string Type,
+         [property: JsonPropertyName("offset")] int Offset,
+         [property: JsonPropertyName("length")] int Length
+    )
+    {
+        [JsonPropertyName("url")] public string? Url { get; init; }
+        [JsonPropertyName("language")] public string? Language { get; init; }
+        [JsonPropertyName("custom_emoji_id")] public string? CustomEmojiId { get; init; }
+        //[field: JsonPropertyName("user")] User? User,
+    };
+}
---
+++ WebSeriveForTrashAPI.Model/Telegram/PayloadsForApi/Message.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using WebSeriveForTrashAPI.Model.Telegram.Models;
+
+namespace WebSeriveForTrashAPI.Model.Telegram.PayloadsForApi
+{
+    public record Message(
+        [field: JsonPropertyName("chat_id")] string ChatId,
+        [field: JsonPropertyName("text")] string Text
+    )
+    {
+        [JsonPropertyName("parse_mode")] public string? ParseMode { get; init; }
+        [JsonPropertyName("entities")] public IReadOnlyList<MessageEntity>? Entities { get; init; }
+        [JsonPropertyName("disable_web_page_preview")] public bool? DisablePreview { get; init; }
+        [JsonPropertyName("disable_notification")] public bool? DisableNotification { get; init; }
+        [JsonPropertyName("protect_content")] public bool? ProtectContent { get; init; }
+        [JsonPropertyName("reply_to_message_id")] public int? ReplyToMessageId { get; init; }
+        [JsonPropertyName("allow_sending_without_reply")] public bool? AllowSendingWithoutReply { get; init; }
+    //[field: JsonPropertyName("reply_markup")] ReplyMarkup? DisablePreview,
+    };
+}
